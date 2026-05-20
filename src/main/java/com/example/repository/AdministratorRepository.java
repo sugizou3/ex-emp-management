f8cf3215eeba5e0f2fe8f88490c4180e3dfb3b67
sugizou3 <sugiyama20010501@gmail.com>
2026-05-20 11:37:40 +0900

    AdministratorRepositoryの仮作成

---
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -0,0 +1,65 @@
+package com.example.repository;
+
+import com.example.domain.Administrator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class AdministratorRepository {
+    /**
+     * レコードとJavaクラスのマッパー
+     */
+    RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER
+            = new BeanPropertyRowMapper<>(Administrator.class);
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    /**
+     * 管理者情報を挿入する.
+     *
+     * @param administrator 管理者ドメイン
+     */
+    public void insert(Administrator administrator) {
+        SqlParameterSource param
+                = new BeanPropertySqlParameterSource(administrator);
+
+        String sql = """
+                INSERT INTO administrators(name,mail_address,password)
+                VALUES (:name,:mailAddress,:password);
+                """;
+
+        template.update(sql, param);
+
+    }
+
+    /**
+     * メールアドレスとパスワードから管理者情報を取得する
+     * （1件も存在しない場合はnullを返す※）.
+     *
+     * @param mailAdress 管理者メールアドレス
+     * @param password   管理者パスワード
+     * @return Administrator 管理者ドメイン
+     */
+    public Administrator findByMailAddressAndPassword(String mailAdress, String password) {
+        String sql = """
+                SELECT id,name,mail_address,password
+                FROM administrators
+                WHERE mail_address = :mailAdress AND password=:password;
+                """;
+        SqlParameterSource param
+                = new MapSqlParameterSource()
+                .addValue("mailAdress", mailAdress)
+                .addValue("password", password);
+        return template.queryForObject(sql, param, ADMINISTRATOR_ROW_MAPPER);
+    }
+    
+
+}
