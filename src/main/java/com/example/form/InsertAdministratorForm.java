632c077cdd4cff4704a46d203ff549e220da03a6
sugizou3 <sugiyama20010501@gmail.com>
2026-05-20 12:07:15 +0900

    Administratorのサービスとインサートフォームを仮作成

---
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -0,0 +1,27 @@
+package com.example.form;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class InsertAdministratorForm {
+    /**
+     * 名前
+     */
+    private String name;
+    /**
+     * メールアドレス
+     */
+    private String mailAdress;
+    /**
+     * パスワード
+     */
+    private String password;
+
+    
+}
---
+++ src/main/java/com/example/service/AdministratorService.java
@@ -0,0 +1,19 @@
+package com.example.service;
+
+import com.example.repository.AdministratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class AdministratorService {
+    /**
+     * AdministratorRepositoryを参照
+     */
+    @Autowired
+    AdministratorRepository repository;
+
+
+}
