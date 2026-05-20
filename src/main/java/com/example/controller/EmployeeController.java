1a96fb7f33aad8bc521be5161eb70ccf2accff03
sugizou3 <sugiyama20010501@gmail.com>
2026-05-20 15:37:50 +0900

    従業員一覧表示の作成

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,7 +1,14 @@
 package com.example.controller;
 
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * 従業員関連機能の処理の制御を行うコントローラー.
@@ -9,5 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping("/show-list")
+    public String showList(Model model) {
+        List<Employee> employeeList = service.showList();
+        model.addAttribute("employeeList", employeeList);
+
+        return "employee/list";
+    }
 
 }
