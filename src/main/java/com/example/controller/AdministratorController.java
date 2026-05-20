0f17e31a470596a0895ad27660fc19fed93bb0b2
sugizou3 <sugiyama20010501@gmail.com>
2026-05-20 12:10:58 +0900

    AdministratorControllerの仮作成 to-insert URLを作成

---
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+
+
+public class AdministratorController {
+    @Autowired
+    AdministratorService service;
+
+    @GetMapping("to-insert")
+    public String toInsert(InsertAdministratorForm form) {
+        return "administrator/insert";
+    }
+
+
+}
